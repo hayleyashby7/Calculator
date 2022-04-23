35a8e480148e90a4105e58df6fe397b9c6a3b07c
Hayley Ashby <hayleyashby7@gmail.com>
2022-04-23 14:45:55 +0100

    Tidied structure of files

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,4 +1,4 @@
-const { add, subtract, multiply, divide } = require("../scripts/calculator");
+const { add, subtract, multiply, divide } = require("../calculator");
 
 describe("Add", () => {
   test("Add 2 numbers together and return result", () => {
