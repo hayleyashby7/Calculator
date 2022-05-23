a7bb897a0eb8f8bff4d3c45e3d65439190c23262
Hayley Ashby <hayleyashby7@gmail.com>
2022-05-23 22:03:01 +0100

    Begin building calculator Object and display handling. Added in button event listeners

--- src/calculator.js
+++ src/calculator.js
@@ -1,20 +1,55 @@
 "use strict";
 
+const calculator = {
+  currentValue: 0,
+  displayValue: 0,
+  firstNumber: 0,
+  secondNumber: 0,
+  operator: "",
+};
+
 calculator();
 
 function calculator() {
-  let displayValue = "0";
-  updateDisplay(displayValue);
+  updateDisplay(calculator.displayValue);
+
+  addButtonClickEvents();
 }
 
 // INTIALISATION FUNCTIONS
 export function addButtonClickEvents() {
-  return true;
+  let buttons = document.querySelectorAll("button");
+  buttons.forEach((element) => {
+    console.log(element);
+    element.addEventListener("click", buttonClick, false);
+  });
 }
 
 // DISPLAY FUNCTIONS
-export function updateDisplay(value) {
-  document.getElementById("display").innerHTML = value;
+export function updateDisplay() {
+  let currentValue = calculator.currentValue;
+
+  console.log(
+    "Current Value: " + currentValue + " and Button Pressed: " + value
+  );
+  if (currentValue == 0) {
+    console.log("current value set to 0");
+    // No value - start with input value
+    document.getElementById("display").innerHTML = value;
+  } else {
+    console.log("current value NOT 0");
+    document.getElementById("display").innerHTML += value;
+  }
+}
+
+// EVENT FUNCTIONS
+function buttonClick() {
+  calculateNewDisplayValue(this.innerHTML);
+}
+
+function calculateNewDisplayValue(value) {
+  calculator.displayValue = value;
+  updateDisplay();
 }
 
 // OPERATOR FUNCTIONS
